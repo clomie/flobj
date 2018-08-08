1c57ac1d134efb967db0ecc78018a552ddcd7d35
clomie <cclomie@gmail.com>
2018-08-08 22:08:06 +0900

    Add jsonsmith class

---
+++ src/main/java/io/jsonsmith/Jsonsmith.java
@@ -0,0 +1,19 @@
+package io.jsonsmith;
+
+import io.jsonsmith.parser.JqPathParser;
+
+public final class Jsonsmith {
+
+    public static Object commit(Object target, String path, Object value) {
+        return new JqPathParser().parse(path).commit(target, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T pick(Object target, String path) {
+        return (T) new JqPathParser().parse(path).pick(target);
+    }
+
+    private Jsonsmith() {
+        //
+    }
+}
